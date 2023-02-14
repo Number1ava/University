6c0cfbe65335256fbcd6c421a6354022e86e5c48
Mirzoavaz <73744057+Number1ava@users.noreply.github.com>
2023-02-14 12:23:00 +0500

    menu done

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
-// $(function () {
-
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  const menuBtns = document.querySelectorAll('.menu__list-btn');
+  menuBtns.forEach(el => {
+    el.addEventListener('click', (e) => {
+      let currentBtn = e.currentTarget;
+      let drop = currentBtn.closest('.menu__list-item').querySelector('.dropdown');
+      drop.classList.toggle('dropdown--active');
+    });
+  });
+});
