41d6b004a31845f3676d3fcd867012bd3eb4c531
Mirzoavaz <73744057+Number1ava@users.noreply.github.com>
2023-02-16 12:21:33 +0500

    add library for counter

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,20 @@ document.addEventListener('DOMContentLoaded', () => {
       drop.classList.toggle('dropdown--active');
     });
   });
+
+  // Jquery - counter
+  let Animation = function () {
+    let hFrom = $(window).scrollTop();
+    let hTo = $(window).scrollTop() + ($(window).height() / 1);
+    if ($(".counter").offset().top > hFrom && $(".counter").offset().top < hTo) {
+      $('.counter__main span').delay(500).spincrement({
+        thousandSeparator: "",
+        duration: 3000
+      });
+    }
+  };
+  $(window).scroll(function () {
+    Animation();
+  });
+  Animation();
 });
